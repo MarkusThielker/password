fc0d177827dfab1bb0f39e7f2331ea029e474a73
Markus Thielker <mail.markus.thielker@gmail.com>
2025-01-16 16:18:29 +0100

    Add authentication to app

--- password/passwordApp.swift
+++ password/passwordApp.swift
@@ -6,12 +6,59 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 @main
 struct passwordApp: App {
+    
+    @State private var isAuthenticated = false
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "This app displays passwords in clear text. Please authenticate using Touch ID or Face ID."
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    isAuthenticated = true
+                } else {
+                    fallBackToPasscode()
+                }
+            }
+        } else {
+            fallBackToPasscode()
+        }
+    }
+    
+    func fallBackToPasscode() {
+        let context = LAContext()
+        let reason = "This app displays passwords in clear text. Please authenticate using your passcode."
+
+        context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reason) { success, authenticationError in
+            if success {
+                isAuthenticated = true
+            } else {
+                // Handle passcode authentication error
+            }
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ListView(viewModel: ListViewModel())
+            VStack {
+                if isAuthenticated {
+                    ListView(viewModel: ListViewModel())
+                } else {
+                    Button("Authenticate") {
+                        authenticate()
+                    }
+                }
+            }
+            .onAppear {
+                authenticate()
+            }
         }
     }
 }
