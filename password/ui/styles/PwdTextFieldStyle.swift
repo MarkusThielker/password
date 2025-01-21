63d2c9e6ebe16da578a0a66c3661a73b2717aaed
Markus Thielker <mail.markus.thielker@gmail.com>
2025-01-21 16:03:00 +0100

    PWD-1: add border to text field style

--- password/ui/styles/PwdTextFieldStyle.swift
+++ password/ui/styles/PwdTextFieldStyle.swift
@@ -11,12 +11,18 @@ struct PwdTextFieldStyle: TextFieldStyle {
     
     @Environment(\.colorScheme) var colorScheme
     
+    private let radius: CGFloat = 12
+    
     func _body(configuration: TextField<Self._Label>) -> some View {
         configuration
             .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
             .background(colorScheme == .dark ? .black : .white)
             .foregroundColor(colorScheme == .dark ? .white : .black)
             .textFieldStyle(.plain)
-            .cornerRadius(12)
+            .cornerRadius(radius)
+            .overlay(
+                RoundedRectangle(cornerRadius: radius)
+                    .stroke(.gray, lineWidth: 1)
+            )
     }
 }
