99c16b368d07b11171026b5e9a3261e28fe6d3c8
Markus Thielker <mail.markus.thielker@gmail.com>
2025-02-08 01:48:19 +0100

    Update text field style

--- password/ui/styles/PwdTextFieldStyle.swift
+++ password/ui/styles/PwdTextFieldStyle.swift
@@ -16,7 +16,7 @@ struct PwdTextFieldStyle: TextFieldStyle {
     func _body(configuration: TextField<Self._Label>) -> some View {
         configuration
             .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
-            .background(colorScheme == .dark ? .black : .white)
+            .background(colorScheme == .dark ? .black.opacity(0.1) : .white)
             .foregroundColor(colorScheme == .dark ? .white : .black)
             .textFieldStyle(.plain)
             .cornerRadius(radius)
